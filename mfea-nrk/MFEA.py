ca0449d18425fc2dd904f79ae0205dc3a6266be6
datvithanh98 <datvithanh98@gmail.com>
2020-02-10 11:19:51 +0700

    update

--- mfea-nrk/MFEA.py
+++ mfea-nrk/MFEA.py
@@ -183,15 +183,41 @@ def solve(fns, pas=1, logger=None, hop_dir='./data/hop', layer_dir='./data/layer
 if __name__ == '__main__':
     logger = init_log()
     os.makedirs('results/mfea', exist_ok=True)
-    hop_dir = './data/hop'
-    layer_dir = './data/layer'
 
+    # rerun = set([tmp.replace('\n', '') for tmp in open('run_hop.txt', 'r').readlines()])
+
+    # hop_dir='./data/hop'
+    # layer_dir='./data/layer'
+    # pases = []
+    # tests = []
+    # # 1 single 3 multi
     # for i in range(10):
-    #     joblib.Parallel(n_jobs=8)(
-    #         joblib.delayed(solve)(fn, pas=i, logger=logger) for fn in sorted(os.listdir(hop_dir))
-    #     )
+    #     rerun_hop = [tmp for tmp in os.listdir(hop_dir) if f'{tmp[:-5]}_{i}.txt' in rerun]
+    #     sets = []
+    #     for j in rerun_hop:
+    #         single = '_'.join(j.split('_')[:-1]) + '.json'
 
-    rerun = set([tmp.replace('\n', '') for tmp in open('mfea_tests.txt', 'r').readlines()])
+    #         r = int(j.split('_')[-3][1:])
+    #         ss = int(j.split('_')[-1][:-5])
+
+    #         splt = j.split('_')
+    #         splt[-1] = str(40-ss) + '.json'
+    #         multi2 = '_'.join(splt)
+            
+    #         splt[-1] = str(ss) + '.json'
+    #         splt[-3] = 'r' + str(75-r)
+    #         multi3 = '_'.join(splt)
+            
+    #         single = os.path.join(layer_dir, single)
+    #         multi1 = os.path.join(hop_dir, j)
+    #         multi2 = os.path.join(hop_dir, multi2)
+    #         multi3 = os.path.join(hop_dir, multi3)
+    #         tests.append([single, multi1, multi2, multi3])
+
+    #     pases = pases + [i] * len(rerun_hop)
+
+    # 3 single 1 multi
+    rerun = set([tmp.replace('\n', '') for tmp in open('run_layer.txt', 'r').readlines()])
 
     hop_dir='./data/hop'
     layer_dir='./data/layer'
@@ -199,32 +225,30 @@ if __name__ == '__main__':
     tests = []
     # 1 single 3 multi
     for i in range(10):
-        rerun_hop = [tmp for tmp in os.listdir(hop_dir) if f'{tmp[:-5]}_{i}.txt' in rerun]
+        rerun_hop = [tmp for tmp in os.listdir(layer_dir) if f'{tmp[:-5]}_{i}.txt' in rerun]
         sets = []
         for j in rerun_hop:
-            single = '_'.join(j.split('_')[:-1]) + '.json'
-
-            r = int(j.split('_')[-3][1:])
-            ss = int(j.split('_')[-1][:-5])
-
             splt = j.split('_')
-            splt[-1] = str(40-ss) + '.json'
-            multi2 = '_'.join(splt)
+
+            r = int(splt[-2][1:])
+            dem = int(splt[0][6:])
+
+            splt[-2] = 'r' + str(75-r)
+            single1 = '_'.join(splt)
+            splt[-2] = 'r' + str(r)
+
+            splt[0] = splt[0][:6] + str(11-dem)
+            single3 = '_'.join(splt)
+            splt[0] = splt[0][:6] + str(dem)
             
-            splt[-1] = str(ss) + '.json'
-            splt[-3] = 'r' + str(75-r)
-            multi3 = '_'.join(splt)
-            
-            single = os.path.join(layer_dir, single)
-            multi1 = os.path.join(hop_dir, j)
-            multi2 = os.path.join(hop_dir, multi2)
-            multi3 = os.path.join(hop_dir, multi3)
-            tests.append([single, multi1, multi2, multi3])
+            multi = j[:-5] + '_0.json'
+            single1 = os.path.join(layer_dir, single1)
+            single2 = os.path.join(layer_dir, j)
+            single3 = os.path.join(layer_dir, single3)
+            multi = os.path.join(hop_dir, multi)
+            tests.append([single1, single2, single3, multi])
 
         pases = pases + [i] * len(rerun_hop)
-
-    # 3 single 1 multi
-    # for i in range(10):
 
     # tests = tests[:1]
     joblib.Parallel(n_jobs=8)(
