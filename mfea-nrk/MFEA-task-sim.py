7c22eaa85c24dfe354a1f1e91810aead23315115
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2020-08-31 15:12:13 +0900

    MFEA-task-sim code

--- mfea-nrk/MFEA-task-sim.py
+++ mfea-nrk/MFEA-task-sim.py
@@ -205,7 +205,7 @@ if __name__ == '__main__':
     print(len(tests))
     print(len(pases))
     
-    joblib.Parallel(n_jobs=16)(
+    joblib.Parallel(n_jobs=32)(
         joblib.delayed(solve)(fn, pas=pas, logger=logger) for fn, pas in zip(tests, pases)
     )
     # for fn, pas in zip(tests, pases):
