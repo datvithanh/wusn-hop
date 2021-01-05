0a7cb92a8b023d0dd3657c47b84621dbfa3645b3
datvithanh98 <datvithanh98@gmail.com>
2021-01-05 09:26:36 +0700

    update

--- scripts/run_rmp_experiment.sh
+++ scripts/run_rmp_experiment.sh
@@ -1,10 +1,12 @@
-python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_uu_pvalue.txt --rmp 0.1 --n-jobs 6
-# python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.1
-# python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.2
-# python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.3
-# python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.4
-# python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.5
-# python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.6
-# python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.7
-# python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.8
-# python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.9
+# python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_uu_pvalue.txt --rmp 0.1 --n-jobs 6
+python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.0
+python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.1
+python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.1
+python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.2
+python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.3
+python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.4
+python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.5
+python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.6
+python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.7
+python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.8
+python mfea-nrk/MFEA-task-sim.py --task-file data/tasks/run_total_uu.txt --rmp 0.9
