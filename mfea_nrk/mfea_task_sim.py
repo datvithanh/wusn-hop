81f041851ca332c424c0ee3240e82bab6f1d4488
datvithanh98 <datvithanh98@gmail.com>
2021-04-02 09:46:57 +0700

    update

--- mfea_nrk/mfea_task_sim.py
+++ mfea_nrk/mfea_task_sim.py
@@ -16,14 +16,158 @@ from constructor.nrk import Nrk
 from utils.logger import init_log
 from utils import config
 
-from mfea_nrk.mfea import run_ga
-
 N_GENS = config.N_GENS
 POP_SIZE = config.POP_SIZE
 CXPB = config.MFEA_CXPB
 TERMINATE = 30
 num_of_relays = config.MAX_RELAYS
 num_hops = config.MAX_HOPS
+
+def init_individual(num_of_relays, num_of_sensors):
+    length = 2 * (num_of_sensors + num_of_relays + 1)
+
+    individual = list(np.random.uniform(0, 1, size=(length,)))
+
+    return individual
+
+def run_ga(fns, flog, logger=None):
+    global num_of_relays, num_hops
+    if logger is None:
+        raise Exception("Error: logger is None!")
+
+    num_tasks = len(fns)
+    inputs = []
+    constructors = []
+    for fn in fns:
+        inputs.append(WusnInput.from_file(fn))
+        if 'layer' in fn:
+            constructors.append(Nrk(inputs[-1], max_relay=num_of_relays, is_hop=False, hop=1000))
+        else:
+            constructors.append(Nrk(inputs[-1], max_relay=num_of_relays, is_hop=True, hop=num_hops))
+
+    num_of_relays = max([inp.num_of_relays for inp in inputs])
+    num_of_sensors = max([inp.num_of_sensors for inp in inputs])
+
+    def transform_genes(individual, task_sensors):
+        first_half = individual[:1 + num_of_relays + task_sensors]
+        second_half = individual[1 + num_of_relays + num_of_sensors:2 + 2*num_of_relays + num_of_sensors + task_sensors]
+
+        return first_half + second_half
+
+    def factorial_rank(pop):
+        factorial_cost = [[constructor.get_loss(transform_genes(indi, inp.num_of_sensors)) for constructor, inp in zip(constructors, inputs)] for indi in pop]
+
+        ranks = []
+        for task in range(num_tasks):
+            rank = zip(np.argsort([tmp[task] for tmp in factorial_cost]), range(len(pop)))
+            rank = [tmp[-1] for tmp in sorted(rank, key=lambda x: x[0])]
+            ranks.append(rank)
+
+        return ranks
+
+    def skill_factor(pop):
+        pop_factorial_rank = factorial_rank(pop)
+
+        pop_skill_factor = [np.argmin([pop_factorial_rank[task][i] for task in range(num_tasks)]) for i in range(len(pop))]
+
+        pop_scalar_fitness = [1/(min([pop_factorial_rank[task][i] for task in range(num_tasks)]) + 1) for i in range(len(pop))]
+
+        # print(pop_skill_factor)
+        # print(sum(pop_skill_factor), len(pop_skill_factor))
+        # factorial rank << -> scalar fitness >> 
+        return pop_skill_factor, pop_scalar_fitness
+
+    def crossover(ind1, ind2):
+        r1, r2 = np.random.randint(0, len(ind1)), np.random.randint(0, len(ind1))
+        r1, r2 = min(r1, r2), max(r1, r2)
+        
+        ind1[:r1], ind2[:r1] = ind2[:r1], ind1[:r1]
+        ind1[r2:], ind2[r2:] = ind2[r2:], ind1[r2:]
+
+        avg = [(tmp1 + tmp2)/2 for tmp1, tmp2 in zip(ind1[r1:r2], ind2[r1:r2])]
+        ind1[r1:r2], ind2[r1:r2] = avg, avg
+
+        return ind1, ind2
+
+    def mutate(ind, mu=config.ELEMENT_MUTATION_MU, sigma=config.ELEMENT_MUTATION_SIGMA, indpb=config.ELEMENT_MUTATION_RATE):
+        size = len(ind)
+
+        for i in range(size):
+            if np.random.random() < indpb:
+                ind[i] += random.gauss(mu, sigma)
+
+        return ind
+
+    def assortive_mating(pop, pop_skill_factor):
+        offsprings = []
+        count1, count2 = 0, 0
+        for _ in range(POP_SIZE//2):
+            idx1, idx2 = np.random.randint(0, POP_SIZE - 1), np.random.randint(0, POP_SIZE - 1)
+            p1, p2 = copy.deepcopy(pop[idx1]), copy.deepcopy(pop[idx2])
+
+            if pop_skill_factor[idx1] == pop_skill_factor[idx2] or np.random.random() < CXPB:
+                count1 += 1
+                offs1, offs2 = crossover(p1, p2)
+                offsprings.extend([offs1, offs2])
+            else:
+                count2 += 1
+                offs1, offs2 = mutate(p1), mutate(p2)
+                offsprings.extend([offs1, offs2])
+
+        return offsprings
+
+    def selection(pop, pop_skill_factor, pop_scalar_fitness):
+        ranking = sorted([(idx, value) for idx, value in enumerate(pop_scalar_fitness)], key=lambda x: -x[-1])
+        new_pop, new_pop_skill_factor = [], []
+
+        for i in range(POP_SIZE):
+            new_pop.append(pop[ranking[i][0]])
+            new_pop_skill_factor.append(pop_skill_factor[ranking[i][0]])
+
+        return new_pop, new_pop_skill_factor
+
+    num_of_relays = max([inp.num_of_relays for inp in inputs])
+    num_of_sensors = max([inp.num_of_sensors for inp in inputs])
+
+    pop = [init_individual(num_of_relays, num_of_sensors) for _ in range(POP_SIZE)]
+
+    pop_skill_factor, _ = skill_factor(pop)
+
+    offspring_pop = assortive_mating(pop, pop_skill_factor)
+
+    hop_individual, layer_individual = None, None
+
+
+    for g in range(N_GENS):
+        flog.write(f'GEN {g} time {int(time.time())}\n')
+
+        offspring_pop = assortive_mating(pop, pop_skill_factor)
+        
+        immediate_pop = pop + offspring_pop
+        
+        pop_skill_factor, pop_scalar_fitness = skill_factor(immediate_pop)
+
+        pop, pop_skill_factor = selection(immediate_pop, pop_skill_factor, pop_scalar_fitness)
+
+        best_indis = [None] * num_tasks
+        for i in range(num_tasks):
+            for j in range(POP_SIZE):
+                if pop_skill_factor[j] == i:
+                    best_indis[i] = pop[j]
+                    break
+            if not best_indis[i]:
+                best_indis[i] = pop[0]
+
+        best_objs = [constructor.get_loss(transform_genes(indi, inp.num_of_sensors)) \
+            for indi, constructor, inp in zip(best_indis, constructors, inputs)]
+
+        for task in range(num_tasks):
+            flog.write(f'{best_objs[task]}\n')
+    
+    if max(best_objs) > 10:
+        return False
+
+    return True
 
 def solve(fns, pas, logger=None):
     print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] solving {fns} pas {pas}')
